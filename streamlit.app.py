f407fadd22acb20fa9a3b079435f5d13a899a27a
bktg1229 <125476048+bktg1229@users.noreply.github.com>
2023-02-20 13:56:24 -0600

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,8 +1,7 @@
-import snowflake.connector
-
-
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
+
+import snowflake.connector
 
 
